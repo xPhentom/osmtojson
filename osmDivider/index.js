eaabd15256e4b0cd253993102e5f1f517e74314e
Wouter Roozeleer <phentom@live.be>
2016-11-07 12:43:45 +0100

    Uses analytics to get boundaries and now uses the folder osmParts

--- osmDivider/index.js
+++ osmDivider/index.js
@@ -4,30 +4,30 @@ var async = require('async');
 
 const execSync = require('child_process').execSync;
 
-// var execSync = require('exec-sync');
-// var sleep = require('sleep');
-
-
 //Storing coordinates boundaries
 var horizontaldivision = [];
 var verticaldivision = [];
 
-RetrieveBoundaries();
-DivideOSM();
+var North; // MaxLat
+var South; // MinLat
+var East; // MaxLon
+var West; // MinLon
 
-//CheckOsmConverter();
+
+CheckOsmConverter();
 
 // Main function
 function CheckOsmConverter() {
     cmd.get(
-        'osmconvert',
+        'osmconvert -v',
         function (data) {
-            if (data.indexOf('not recognized') == -1) {
+            if (data.indexOf('not recognized') != -1) {
                 console.log("It seems like osmconvert isn't installed yet");
                 return;
             } else {
                 Setup();
-                RetrieveBoundaries();
+                CreateBoundaries();
+                DivideOSM();
             }
         }
     );
@@ -35,19 +35,17 @@ function CheckOsmConverter() {
 
 //Setting up essential parts
 function Setup() {
+    console.log("Creating osmParts folder");
     cmd.run('mkdir osmParts');
 }
 
 // Retrieving Data
 
-function RetrieveBoundaries() {
+function CreateBoundaries() {
 
     // Setting startdata 
 
-    var North = 51.49;
-    var South = 49.49;
-    var East = 6.40;
-    var West = 2.54;
+    RetrieveCoordinates();
 
     console.log("Startwaarden: " + " North: " + North + ", South: " + South + ", East: " + East + ", West: " + West);
 
@@ -92,6 +90,31 @@ function RetrieveBoundaries() {
 
 }
 
+function RetrieveCoordinates() {
+    execSync('osmconvert belgium-latest.osm --out-statistics > statistics.txt');
+
+    var array = fs.readFileSync("statistics.txt").toString().split('\n');
+
+    for (var i = 0; i < array.length; i++) {
+
+        if (array[i].indexOf("lon min") > -1) {
+            West = parseFloat(array[i].substr(array[i].indexOf(":") + 1));
+            
+        } else if (array[i].indexOf("lon max") > -1) {
+            East = parseFloat(array[i].substr(array[i].indexOf(":") + 1));
+           
+        } else if (array[i].indexOf("lat min") > -1) {
+            South = parseFloat(array[i].substr(array[i].indexOf(":") + 1));
+            
+        } else if (array[i].indexOf("lat max") > -1) {
+            North = parseFloat(array[i].substr(array[i].indexOf(":") + 1));
+            
+        }
+    }
+}
+
+
+
 
 //Function to divide the original OSM file into multiple, smaller OSM files and stores it in the folder 'osmParts'
 
@@ -107,16 +130,9 @@ function DivideOSM() {
             var South = verticaldivision[i + 1];
             var East = horizontaldivision[j + 1];
 
-            console.log('osmconvert belgium-latest.osm -b=' + North + ',' + West + ',' + South + ',' + East + ' -o=belgium' + filecounter + '.osm');
+            console.log('osmconvert belgium-latest.osm -b=' + North + ',' + West + ',' + South + ',' + East + ' -o=osmParts/belgium' + filecounter + '.osm --verbose');
 
-
-            // cmd.run('osmconvert belgium-latest.osm -b=' + North + ',' + West + ',' + South + ',' + East + ' -o=belgium' + filecounter + '.osm');
-
-            // var test = execSync('osmconvert belgium-latest.osm -b=' + North + ',' + West + ',' + South + ',' + East + ' -o=belgium' + filecounter + '.osm');
-
-            execSync('osmconvert belgium-latest.osm -b=' + North + ',' + West + ',' + South + ',' + East + ' -o=belgium' + filecounter + '.osm');
-
-            // sleep.sleep(5);
+            execSync('osmconvert belgium-latest.osm -b=' + North + ',' + West + ',' + South + ',' + East + ' -o=osmParts/belgium' + filecounter + '.osm --verbose');
 
             filecounter++;
 
