d15c8f5bc1a5cf2ddb2ba54b72fa70b29a83d20f
Wouter Roozeleer <phentom@live.be>
2016-11-16 17:51:05 +0100

    No need to convert osm.pbf to osm, divides immediatly. Small countries fixed. Empty osmparts are common. TODO: post to Solr

--- osmDivider/index.js
+++ osmDivider/index.js
@@ -9,10 +9,11 @@ const execSync = require('child_process').execSync;
 var horizontaldivision = [];
 var verticaldivision = [];
 
-var North; // MaxLat
-var South; // MinLat
-var East; // MaxLon
-var West; // MinLon
+var max_lat; // MaxLat
+var min_lat; // MinLat
+var max_lon; // MaxLon
+var min_lon; // MinLon
+var resultingfilename;
 
 var osmconvert = true;
 var osmosis = true;
@@ -68,8 +69,9 @@ function Startup() {
         console.log("All applications are installed, let's go");
         Setup();
         readosmpbffolder();
-        OSMDivider();
-        ConvertOsmToGeojson();
+	
+        //OSMDivider();
+        //ConvertOsmToGeojson();
         //SendToSolr(); TODO: still needs to be tested
     }
 }
@@ -93,16 +95,19 @@ function readosmpbffolder() {
             console.log("Looks like there are no osm.pbf files in the osmpbf folder");
             return;
         }
+
+	console.log("The next files are found");
+
         files.forEach(filename => {
             console.log(filename);
-            var osmfilename = filename.replace('osm.pbf', 'osm');
-            console.log("converting " + filename + " to " + osmfilename);
-            execSync("osmosis --read-pbf osmpbf/" + filename + " --write-xml osm/" + osmfilename);
-        });
+	    resultingfilename = filename.replace('-latest.osm.pbf','');
+	    CreateBoundaries(filename);
+	});
     })
+   
 };
 
-function OSMDivider() {
+/*function OSMDivider() {
     fs.readdir("osm", (err, files) => {
         if (files.length == 0) {
             console.log("Looks like there are no osm files in the osm folder");
@@ -113,7 +118,7 @@ function OSMDivider() {
             DivideOSM(filename);
         });
     })
-}
+}*/
 
 // Retrieving Data
 
@@ -123,44 +128,51 @@ function CreateBoundaries(Filename) {
 
     RetrieveCoordinates(Filename);
 
-    console.log("Startwaarden: " + " North: " + North + ", South: " + South + ", East: " + East + ", West: " + West);
+    console.log("Starting values: " + " max_lat: " + max_lat + ", min_lat: " + min_lat + ", max_lon: " + max_lon + ", min_lon: " + min_lon);
 
-    var height = North - South;
-    var width = East - West;
+    var height = max_lat - min_lat;
+    var width = max_lon - min_lon;
 
-    var multiply = width * height;
+    var surface = width * height;
 
-    var divider = parseInt(multiply, 10);
+    var divider = parseInt(surface);
 
-    // dividing country from North to South
+    // dividing country from max_lat to min_lat
 
     var heightpart = height / divider;
 
-    var startNorth = South;
+    var startmax_lat = min_lat;
 
 
 
     for (i = 0; i <= divider; i++) {
-        horizontaldivision.push((Math.round(startNorth * 100) / 100).toFixed(2));
-        startNorth += heightpart;
+        horizontaldivision.push((Math.round(startmax_lat * 100) / 100).toFixed(2));
+        //horizontaldivision.push(startmax_lat);
+	startmax_lat += heightpart;
     }
 
     console.log("Horizontale verdeling: " + horizontaldivision);
 
-    // dividing country from West to East
+    // dividing country from min_lon to max_lon
 
     var widthpart = width / divider;
 
-    var startEast = West;
+    var startmax_lon = min_lon;
 
 
 
     for (i = 0; i <= divider; i++) {
-        verticaldivision.push((Math.round(startEast * 100) / 100).toFixed(2));
-        startEast += widthpart;
+        verticaldivision.push((Math.round(startmax_lon * 100) / 100).toFixed(2));
+        //verticaldivision.push(startmax_lon);
+	startmax_lon += widthpart;
     }
 
     console.log("Verticale verdeling: " + verticaldivision);
+
+    DivideOSM(Filename, max_lat, min_lat, max_lon, min_lon);
+
+    ConvertOsmToGeojson();
+
 }
 
 
@@ -168,7 +180,9 @@ function CreateBoundaries(Filename) {
 function RetrieveCoordinates(Filename) {
 
     // osmconvert --out-statistics return the north, east, south and west values of the osm.pbf file
-    execSync('osmconvert osm/' + Filename + ' --out-statistics > statistics.txt');
+    execSync('osmconvert osmpbf/' + Filename + ' --out-statistics > statistics.txt');
+
+    execSync('cat statistics.txt');
 
     //To make the script system independant, we quickly create a small file from which we will get the coordinates
     var array = fs.readFileSync("statistics.txt").toString().split('\n');
@@ -176,16 +190,17 @@ function RetrieveCoordinates(Filename) {
     for (var i = 0; i < array.length; i++) {
 
         if (array[i].indexOf("lon min") > -1) {
-            West = parseFloat(array[i].substr(array[i].indexOf(":") + 1));
+            min_lon = parseFloat(array[i].substr(array[i].indexOf(":") + 1));
 
         } else if (array[i].indexOf("lon max") > -1) {
-            East = parseFloat(array[i].substr(array[i].indexOf(":") + 1));
+            max_lon = parseFloat(array[i].substr(array[i].indexOf(":") + 1));
+
 
         } else if (array[i].indexOf("lat min") > -1) {
-            South = parseFloat(array[i].substr(array[i].indexOf(":") + 1));
+            min_lat = parseFloat(array[i].substr(array[i].indexOf(":") + 1));
 
         } else if (array[i].indexOf("lat max") > -1) {
-            North = parseFloat(array[i].substr(array[i].indexOf(":") + 1));
+            max_lat = parseFloat(array[i].substr(array[i].indexOf(":") + 1));
 
         }
     }
@@ -197,26 +212,41 @@ function RetrieveCoordinates(Filename) {
 //Function to divide the original OSM file into multiple, smaller OSM files and stores it in the folder 'osmParts'
 
 //Example osmconvert command: osmconvert belgium-latest.osm -b=2.54,49.49,3.09,49.78 -o=nuernberg.osm
-function DivideOSM(Filename) {
+function DivideOSM(Filename, _maxlat, _minlat, _maxlon, _minlon) {
 
     var filecounter = 0;
 
+    if (verticaldivision.length ==  1 ) {
+	console.log("This country is too small, using boundaries instead");	
+	console.log('osmconvert osmpbf/' + Filename + ' -b=' + _maxlat + ',' + _minlon + ',' + _minlat + ',' + _maxlon + ' -o=osmparts/' + resultingfilename + '.osm --verbose');
+  	execSync('osmosis --read-pbf file=osmpbf/' + Filename  + ' --write-xml osmparts/' + resultingfilename + '.osm' );
+	}
+	else {
+
     //Giving the variables the right value
+
     for (i = 0; i < verticaldivision.length - 1; i++) {
         for (j = 0; j < horizontaldivision.length - 1; j++) {
-            var North = verticaldivision[i];
-            var West = horizontaldivision[j];
-            var South = verticaldivision[i + 1];
-            var East = horizontaldivision[j + 1];
+            var max_lat = verticaldivision[i];
+            var min_lon = horizontaldivision[j];
+            var min_lat = verticaldivision[i + 1];
+            var max_lon = horizontaldivision[j + 1];
 
-            console.log('osmconvert osm/' + Filename + ' -b=' + North + ',' + West + ',' + South + ',' + East + ' -o=osmParts/belgium' + filecounter + '.osm --verbose');
-
-            execSync('osmconvert osm/' + Filename + ' -b=' + North + ',' + West + ',' + South + ',' + East + ' -o=osmParts/belgium' + filecounter + '.osm --verbose');
+	    //console.log('osmosis --read-pbf file=osmpbf/' + Filename  + ' --bounding-box top=' + min_lat  + ' left=' + max_lon + ' bottom=' + max_lat + ' right=' + min_lon + ' --write-xml osmparts/' + resultingfilename + filecounter + '.osm');
+            console.log('osmconvert osmpbf/' + Filename + ' -b=' + max_lat + ',' + min_lon + ',' + min_lat + ',' + max_lon + ' -o=osmparts/' + resultingfilename + filecounter + '.osm --verbose');
+	    console.log("Dit is de filename " + Filename);
+	    //execSync('osmosis --read-pbf file=osmpbf/' + Filename  + ' --bounding-box top=' + min_lat  + ' left=' + min_lon + ' bottom=' + max_lat + ' right=' + max_lon + ' --write-xml osmparts/' + resultingfilename + filecounter + '.osm' );           
+           execSync('osmconvert osmpbf/' + Filename + ' -b=' + max_lat + ',' + min_lon + ',' + min_lat + ',' + max_lon + ' -o=osmparts/' + resultingfilename + filecounter + '.osm');
+            console.log(resultingfilename);
 
             filecounter++;
 
         }
     }
+}
+  verticaldivision = [];
+  horizontaldivision = [];
+
 }
 
 
